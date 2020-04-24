d618d83d2aa89a8682b5eb076a77b8698d9e0226
yu-kurih <yu.k.development@gmail.com>
2020-04-24 15:22:01 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\User; //追加
 
-class UserController extends Controller
+class UsersController extends Controller
 {
     public function index()
     {
@@ -20,9 +20,15 @@ class UserController extends Controller
     public function show($id)
     {
         $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        return view('users.show', [
+        $data = [
             'user' => $user,
-        ]);
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
     }
 }
